bb9175dfa3b2c9cddbe61c55265ebacb73ae8c20
theelgreco <stelios.kakoliris@yahoo.com>
2025-07-19 15:21:05 +0100

    removed unused function

--- src/api/ideas.ts
+++ src/api/ideas.ts
@@ -1,13 +1,6 @@
-import { MOCK_API_DELAY_AMOUNT } from "@/utils/constants";
-import delay from "@/utils/delay";
-
 export interface Idea {
     name: string;
     description: string;
     createdAt: string;
     lastModified: string;
-}
-
-async function fetchIdeas() {
-    await delay(MOCK_API_DELAY_AMOUNT);
 }
