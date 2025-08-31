b0242158e80ac87527e78ad7260b50a691030069
theelgreco <stelios.kakoliris@yahoo.com>
2025-08-31 11:22:12 +0100

    Colocated sidebar state

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,121 +2,27 @@ import "./App.css";
 import IdeaBoard from "@/components/IdeaBoard/IdeaBoard";
 import SideBar from "../components/SideBar/SideBar";
 import { useEffect, useState } from "react";
-import { type Collection, deleteCollection, getCollections, patchCollection, postCollection } from "@/api/collections";
+import { type Collection } from "@/api/collections";
 import { Toaster } from "@/components/ui/sonner";
-import { toast } from "sonner";
-import { UniqueConstraintError } from "@/utils/errors";
 
 function App() {
-    const [collections, setCollections] = useState<Collection[] | null>(null);
     const [selectedCollection, setSelectedCollection] = useState<Collection | null>(null);
-    const [isAddingCollection, setIsAddingCollection] = useState(false);
-    const [isSubmittingCollection, setIsSubmittingCollection] = useState(false);
     const [isSideBarOpen, setIsSideBarOpen] = useState(false);
-    const menuItems = [
-        { label: "All Ideas", value: "all", editable: false },
-        ...(collections || []).map((el) => ({
-            label: el.name,
-            value: el.id,
-            editable: true,
-        })),
-    ];
-
-    async function fetchCollections() {
-        try {
-            const response = await getCollections();
-            setCollections(response.data);
-        } catch {
-            toast.error("An error occurred while fetching collections. Try again.");
-        }
-    }
-
-    async function createCollection(name: string) {
-        try {
-            const response = await postCollection({ name });
-            setCollections((prev) => {
-                if (prev) {
-                    return [...prev, response.data];
-                }
-                return [response.data];
-            });
-            setSelectedCollection(response.data);
-        } catch (err: unknown) {
-            if (err instanceof UniqueConstraintError) {
-                toast.error(err.message);
-            }
-            console.error(err);
-        }
-    }
-
-    async function handleOnCreate(name: string) {
-        setIsSubmittingCollection(true);
-
-        if (name) {
-            await createCollection(name);
-        }
-
-        setIsAddingCollection(false);
-        setIsSubmittingCollection(false);
-    }
-
-    async function updateCollection(value: string, id: string) {
-        try {
-            const response = await patchCollection({ name: value }, { id });
-
-            if (collections) {
-                setCollections([...collections.filter((el) => el.id !== id), response.data]);
-
-                if (selectedCollection?.id === id) {
-                    setSelectedCollection(response.data);
-                }
-            }
-        } catch (err: unknown) {
-            if (err instanceof Error) {
-                toast.error(err.message);
-            }
-        }
-    }
-
-    async function removeCollection(id: string) {
-        try {
-            await deleteCollection({ id });
-            if (collections) {
-                setCollections(collections.filter((el) => el.id !== id));
-                if (selectedCollection && selectedCollection.id === id) {
-                    setSelectedCollection(null);
-                }
-            }
-        } catch (err: unknown) {
-            if (err instanceof Error) {
-                toast.error(err.message);
-            }
-        }
-    }
 
     useEffect(() => {
         if (window.matchMedia("(prefers-color-scheme: dark)")) {
             document.body.classList.add("dark");
         }
-
-        fetchCollections();
     }, []);
 
     return (
         <>
             <main className="flex w-full h-full sm:p-5! gap-5 bg-primary-background-color">
                 <SideBar
-                    menuItems={menuItems}
+                    isSideBarOpen={isSideBarOpen}
+                    setIsSideBarOpen={() => setIsSideBarOpen(false)}
                     selectedCollection={selectedCollection}
-                    isAddingCollection={isAddingCollection}
-                    isSubmittingCollection={isSubmittingCollection}
-                    isOpen={isSideBarOpen}
-                    onSelectCollection={(value) => setSelectedCollection(collections?.find((el) => el.id === value) || null)}
-                    onCreateCollection={handleOnCreate}
-                    onEditCollection={updateCollection}
-                    onDeleteCollection={removeCollection}
-                    onClickAddCollection={() => setIsAddingCollection(true)}
-                    onCloseSideBar={() => setIsSideBarOpen(false)}
+                    setSelectedCollection={setSelectedCollection}
                 />
                 <IdeaBoard selectedCollection={selectedCollection} onOpenSideBar={() => setIsSideBarOpen(true)} />
             </main>
--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,28 +1,113 @@
 import Button from "@/components/Button/Button";
 import { MdAdd } from "react-icons/md";
 import Menu from "@/components/Menu/Menu";
-import type { SideBarProps } from "./types";
 import clsx from "clsx";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import { deleteCollection, getCollections, patchCollection, postCollection } from "@/api/collections";
+import type { Collection } from "@/api/collections";
+import { UniqueConstraintError } from "@/utils/errors";
 
-export default function SideBar({
-    menuItems,
-    selectedCollection,
-    isAddingCollection,
-    isSubmittingCollection,
-    isOpen,
-    onSelectCollection,
-    onCreateCollection,
-    onEditCollection,
-    onDeleteCollection,
-    onClickAddCollection,
-    onCloseSideBar,
-}: SideBarProps) {
+export interface SideBarProps {
+    isSideBarOpen: boolean;
+    setIsSideBarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    selectedCollection: Collection | null;
+    setSelectedCollection: React.Dispatch<React.SetStateAction<Collection | null>>;
+}
+
+export default function SideBar({ isSideBarOpen, setIsSideBarOpen, selectedCollection, setSelectedCollection }: SideBarProps) {
+    const [collections, setCollections] = useState<Collection[] | null>(null);
+    const [isAddingCollection, setIsAddingCollection] = useState(false);
+    const [isSubmittingCollection, setIsSubmittingCollection] = useState(false);
+    const menuItems = [
+        { label: "All Ideas", value: "all", editable: false },
+        ...(collections || []).map((el) => ({
+            label: el.name,
+            value: el.id,
+            editable: true,
+        })),
+    ];
+
+    async function createCollection(name: string) {
+        try {
+            const response = await postCollection({ name });
+            setCollections(collections ? [...collections, response.data] : [response.data]);
+            setSelectedCollection(response.data);
+        } catch (err: unknown) {
+            if (err instanceof UniqueConstraintError) {
+                toast.error(err.message);
+            }
+            console.error(err);
+        }
+    }
+
+    async function handleOnCreate(name: string) {
+        setIsSubmittingCollection(true);
+
+        if (name) {
+            await createCollection(name);
+        }
+
+        setIsAddingCollection(false);
+        setIsSubmittingCollection(false);
+    }
+
+    async function updateCollection(name: string, id: string) {
+        try {
+            const response = await patchCollection({ name }, { id });
+
+            setCollections((prevCollections) => {
+                return (prevCollections || []).map((collection) => {
+                    if (collection.id === id) {
+                        collection.name = name;
+                    }
+
+                    return collection;
+                });
+            });
+            setSelectedCollection(response.data);
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                toast.error(err.message);
+            }
+        }
+    }
+
+    async function removeCollection(id: string) {
+        try {
+            await deleteCollection({ id });
+            if (collections) {
+                setCollections(collections.filter((el) => el.id !== id));
+                if (selectedCollection && selectedCollection.id === id) {
+                    setSelectedCollection(null);
+                }
+            }
+        } catch (err: unknown) {
+            if (err instanceof Error) {
+                toast.error(err.message);
+            }
+        }
+    }
+
+    useEffect(() => {
+        async function fetchCollections() {
+            try {
+                const response = await getCollections();
+                setCollections(response.data);
+            } catch {
+                toast.error("An error occurred while fetching collections. Try again.");
+            }
+        }
+
+        fetchCollections();
+    }, []);
+
     return (
         <>
             <div
                 className={clsx(
                     "h-full flex flex-col justify-between sm:gap-3 sm:w-[400px] max-sm:fixed max-sm:bg-inherit max-sm:z-40 max-sm:overflow-hidden max-sm:transition-all max-sm:-translate-x-full max-sm:w-2/3 max-sm:p-3",
-                    { "max-sm:translate-x-0": isOpen }
+                    { "max-sm:translate-x-0": isSideBarOpen }
                 )}
             >
                 <div className="max-h-full overflow-y-auto p-1">
@@ -30,16 +115,16 @@ export default function SideBar({
                         items={menuItems}
                         selectedItem={selectedCollection ? selectedCollection.id : "all"}
                         isAdding={isAddingCollection}
-                        onCreate={onCreateCollection}
-                        onSelect={(item) => onSelectCollection(item.value)}
-                        onEdit={(item, name) => onEditCollection(name, item.value)}
-                        onDelete={({ value }) => onDeleteCollection(value)}
+                        onCreate={(name) => handleOnCreate(name)}
+                        onSelect={(menuItem) => setSelectedCollection(collections?.find((el) => el.id === menuItem.value) || null)}
+                        onEdit={(menuItem, name) => updateCollection(name, menuItem.value)}
+                        onDelete={(menuItem) => removeCollection(menuItem.value)}
                     />
                 </div>
                 <div className="flex flex-col gap-2">
                     <hr className="border-stone-700" />
                     <Button
-                        onClick={onClickAddCollection}
+                        onClick={() => setIsAddingCollection(true)}
                         text="Add Collection"
                         Icon={MdAdd}
                         variant="plain"
@@ -49,7 +134,9 @@ export default function SideBar({
                     />
                 </div>
             </div>
-            {isOpen && <div className="sm:hidden fixed w-full h-full bg-black/70 z-39" onClick={onCloseSideBar}></div>}
+            {isSideBarOpen && (
+                <div className="sm:hidden fixed w-full h-full bg-black/70 z-39" onClick={() => setIsSideBarOpen(false)}></div>
+            )}
         </>
     );
 }
