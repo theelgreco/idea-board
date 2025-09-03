ec669b1149f4d0a4981a19f7941563c27cc0ff6e
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-03 22:03:08 +0100

    updates tests

--- __tests__/e2e/ideas.spec.ts
+++ __tests__/e2e/ideas.spec.ts
@@ -36,25 +36,46 @@ test.describe("Idea Board E2E", () => {
     });
 
     test("should add, edit, persist, and delete ideas", async ({ page }) => {
+        // Ensure "new idea" button is hidden after being clicked
+        const newIdeaButton = page.getByRole("button", { name: "New Idea" });
+        await expect(newIdeaButton).toBeVisible();
+        await newIdeaButton.click();
+        await expect(newIdeaButton).toBeHidden();
+
         // Add name
-        await page.getByRole("button", { name: "New Idea" }).click();
         await page.getByRole("textbox", { name: "Enter idea name" }).fill("hello");
         await page.getByRole("textbox", { name: "Enter idea name" }).press("Enter");
+        await expect(page.getByRole("textbox", { name: "Enter idea name" })).toHaveValue("hello");
+
         // Add description
         await page.getByRole("textbox", { name: "What's your idea?" }).click();
         await page.getByRole("textbox", { name: "What's your idea?" }).fill("world");
         await page.getByRole("textbox", { name: "What's your idea?" }).press("Shift+Enter");
+        await expect(page.getByRole("textbox", { name: "What's your idea?" })).toHaveValue("world");
+
         // Edit name
         await page.getByRole("textbox", { name: "Enter idea name" }).click();
         await page.getByRole("textbox", { name: "Enter idea name" }).fill("hellojwfnjff");
         await page.getByRole("textbox", { name: "Enter idea name" }).press("Enter");
+        await expect(page.getByRole("textbox", { name: "Enter idea name" })).toHaveValue("hellojwfnjff");
+
         // Edit description
         await page.getByRole("textbox", { name: "What's your idea?" }).click();
         await page.getByRole("textbox", { name: "What's your idea?" }).fill("worldjwdfnjj");
         await page.getByRole("textbox", { name: "What's your idea?" }).press("Shift+Enter");
+        await expect(page.getByRole("textbox", { name: "What's your idea?" })).toHaveValue("worldjwdfnjj");
+
         // Reload page and ensure idea is still there
         await page.reload();
+        await expect(page.getByRole("textbox", { name: "Enter idea name" })).toHaveValue("hellojwfnjff");
+        await expect(page.getByRole("textbox", { name: "What's your idea?" })).toHaveValue("worldjwdfnjj");
+
+        await expect(page.getByRole("article")).toHaveCount(1);
+        await checkNumberOfIdeasInLocalStorage(page, 1);
+
         // Delete idea
         await page.getByRole("article").locator("header").getByRole("button").click();
+        await expect(page.getByRole("article")).toHaveCount(0);
+        await checkNumberOfIdeasInLocalStorage(page, 0);
     });
 });
