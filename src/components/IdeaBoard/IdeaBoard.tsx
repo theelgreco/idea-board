2f2d22a58d42c328b25d3eb8ac6eec7262a5fd55
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-07 22:41:51 +0100

    Adds header and content components to IdeaBoard

--- src/components/IdeaBoard/IdeaBoard.tsx
+++ src/components/IdeaBoard/IdeaBoard.tsx
@@ -1,7 +1,4 @@
 import { useCallback, useEffect, useState } from "react";
-import IdeaCard from "../IdeaCard/IdeaCard";
-import Button from "../Button/Button";
-import { MdMenu } from "react-icons/md";
 import {
     deleteIdea,
     getIdeas,
@@ -12,15 +9,21 @@ import {
     type IdeaPutData,
     type IdeaPutParams,
 } from "@/api/ideas";
-import type { IdeaBoardProps, OrderChoices, SortByCollection } from "./types";
 import type { IdeaCardSaveArgs } from "../IdeaCard/IdeaCard";
-import SortControls from "../SortControls/SortControls";
-import NewIdeaButton from "../NewIdeaButton/NewIdeaButton";
+import { type OrderChoices, type SortByCollection } from "../SortControls/SortControls";
+import type { Collection } from "@/api/collections";
+import IdeaBoardHeader from "./IdeaBoardHeader";
+import IdeaBoardContent from "./IdeaBoardContent";
 
 const sortByOptions: SortByCollection[] = [
     { label: "Created At", value: "createdAt" },
     { label: "Name", value: "name" },
 ];
+
+interface IdeaBoardProps {
+    selectedCollection: Collection | null;
+    setIsSideBarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
 export default function IdeaBoard({ selectedCollection, setIsSideBarOpen }: IdeaBoardProps) {
     const [ideas, setIdeas] = useState<Idea[] | null>(null);
@@ -84,49 +87,24 @@ export default function IdeaBoard({ selectedCollection, setIsSideBarOpen }: Idea
 
     return (
         <main className="relative flex flex-col w-full h-full overflow-hidden sm:rounded-2xl sm:bg-secondary-background-color after:absolute after:w-full after:h-full sm:after:inset-shadow-[0_0_8px_2px_rgba(0,0,0,0.75)] after:rounded-2xl after:pointer-events-none">
-            <div className="w-full flex justify-between px-5 py-3 sm:px-8 sm:py-5 bg-[#222222]">
-                <div className="flex items-center gap-5 max-w-full overflow-hidden">
-                    <Button
-                        Icon={MdMenu}
-                        iconProps={{ size: 24 }}
-                        variant="plain"
-                        className="sm:hidden! cursor-pointer min-w-[24px]! p-0!"
-                        onClick={() => setIsSideBarOpen(true)}
-                    />
-                    <h1 className="font-semibold text-2xl text-ellipsis text-nowrap overflow-hidden">
-                        {selectedCollection?.name || "All Ideas"}
-                    </h1>
-                </div>
-                <SortControls
-                    order={order}
-                    setOrder={setOrder}
-                    sortByOptions={sortByOptions}
-                    selectedSortByOption={selectedSortByOption}
-                    setSelectedSortByOption={setSelectedSortByOption}
-                />
-            </div>
-            <div className="flex gap-10 flex-wrap px-8! py-5! overflow-auto">
-                {!isAdding && <NewIdeaButton setIsAdding={setIsAdding} />}
-                {isAdding && (
-                    <IdeaCard
-                        isNew={true}
-                        onSave={(newIdea) => {
-                            createIdea(newIdea);
-                            setIsAdding(false);
-                        }}
-                    />
-                )}
-                {ideas &&
-                    ideas.map((idea) => (
-                        <IdeaCard
-                            key={idea.id}
-                            idea={idea}
-                            autoFocusDescription={idea.id === lastAddedIdeaId}
-                            onSave={(data) => editIdea(data, { id: idea.id })}
-                            onDelete={() => removeIdea({ id: idea.id })}
-                        />
-                    ))}
-            </div>
+            <IdeaBoardHeader
+                selectedCollection={selectedCollection}
+                order={order}
+                sortByOptions={sortByOptions}
+                selectedSortByOption={selectedSortByOption}
+                setSelectedSortByOption={setSelectedSortByOption}
+                setOrder={setOrder}
+                setIsSideBarOpen={setIsSideBarOpen}
+            />
+            <IdeaBoardContent
+                ideas={ideas}
+                isAdding={isAdding}
+                lastAddedIdeaId={lastAddedIdeaId}
+                setIsAdding={setIsAdding}
+                createIdea={createIdea}
+                editIdea={editIdea}
+                removeIdea={removeIdea}
+            />
         </main>
     );
 }
