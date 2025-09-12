a6e2dde3d3a65d2e00a0acfd6ecd19feaf66256c
theelgreco <stelios.kakoliris@yahoo.com>
2025-09-12 21:04:45 +0100

    adds new collection test

--- __tests__/e2e/collections.spec.ts
+++ __tests__/e2e/collections.spec.ts
@@ -16,5 +16,10 @@ test.describe("Collections E2E", () => {
         await expect(addCollectionButton).toBeEnabled();
         await addCollectionButton.click();
         await expect(addCollectionButton).toBeDisabled();
+
+        // Add collection
+        page.getByRole("textbox", { name: "Enter Collection Name" }).fill("My collection");
+        await page.getByRole("textbox", { name: "Enter Collection Name" }).press("Enter");
+        await checkNumberOfCollectionsInLocalStorage(page, 1);
     });
 });
